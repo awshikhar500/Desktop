f012e8e96b23e03ffeffa194c00b7b459234821f
awshikhar500 <awshikhar500@gmail.com>
2023-01-30 19:39:23 +0530

    qwe

---
+++ untitled folder/circle.py
@@ -0,0 +1,9 @@
+d=int(input('diameter:'))
+r=d/2
+c=2*3.145*r
+a=3.145*r*r
+print("area and circumference calculate")
+print(f'{d=}')
+print(f'{r=}')
+print(f'area{a=}')
+print(f'circumference{c=}')
---
+++ untitled folder/si.py
@@ -0,0 +1,10 @@
+# si = p * r * t / 100
+p= int(input('Principal:'))
+r= int(input('Rate of Interest:'))
+t= int(input('Time:'))
+si= p*r*t/100
+print("Simple Interest Calc")
+print(f'{p=}')
+print(f'{r=}')
+print(f'{t=}')
+print(f'Simple interest; {si}')
